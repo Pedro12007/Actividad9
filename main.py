473bbbcb35006b3e97e9de14b0fb5fdfba026c4f
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-07-25 15:57:28 -0600

    Estructura del menú

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 peliculas = []
 
+while True:
+    print('Opciones:')
+    print('1. Agregar películas.')
+    print('2. Mostrar todas las películas registradas.')
+    print('3. Buscar películas por género.')
+    print('4. Eliminar una película por título.')
+    print('5. Ver estadísticas del catálogo.')
+    print('6. Salir del programa')
+
+    option = input('Ingrese la opción que desea: ')
+
+    match option:
+        case '1':
+            pass
+
+        case '2':
+            pass
+
+        case '3':
+            pass
+
+        case '4':
+            pass
+
+        case '5':
+            pass
+
+        case '6':
+            print('Saliendo del programa...')
+            break
+
+        case _:
+            print('Opción inválida. Intente nuevamente.\n')
