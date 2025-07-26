5b8a04e47d1f45b631375c66b06bb293079e4079
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-07-25 19:46:15 -0600

    Opción 1 y excepciones para ingreso de int

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 peliculas = []
 
+def int_input_exception(text):
+    while True:
+        try:
+            value = int(input(f'Ingrese {text}: '))
+            return value
+        except ValueError:
+            print('Debe ser un número.\n')
+
+def add_movie(amount):
+    movies_list = []
+    for i in range(amount):
+        title = input('Ingrese el título de la película')
+        year = int_input_exception('el año de la película')
+        genre = input('Ingrese el género de la película: ')
+
+        movie_details = [title, year, genre]
+        movies_list.append(movie_details)
+    return movies_list
+
 while True:
     print('Opciones:')
     print('1. Agregar películas.')
@@ -13,10 +32,13 @@ while True:
 
     match option:
         case '1':
-            pass
+            movies_amount = int_input_exception('cuantas películas desea ingresar')
+            movies = add_movie(movies_amount)
+            peliculas.extend(movies)
 
         case '2':
-            pass
+            for i in range(len(peliculas)):
+                 pass
 
         case '3':
             pass
